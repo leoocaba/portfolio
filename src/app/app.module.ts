21513445d8401b13df68cdc501e23b83e0c71a01
leoocaba <leoo.caballero@gmail.com>
2022-07-26 20:01:26 -0300

    Componentes que comprenden 'Sócalo superior' y 'Header'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SocaloSuperiorComponent } from './components/socalo-superior/socalo-superior.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoIdevComponent } from './components/logo-idev/logo-idev.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { MenuUlComponent } from './components/menu-ul/menu-ul.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocaloSuperiorComponent,
+    HeaderComponent,
+    LogoIdevComponent,
+    LogoAPComponent,
+    MenuUlComponent
   ],
   imports: [
     BrowserModule
