9fe73d0b61c2760aed378d63ab54b2357213ce8d
leoocaba <leoo.caballero@gmail.com>
2022-08-01 18:55:58 -0300

    Se agregó routerLinkActive y scrolltop al montar componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'iDev - Leonel Caballero';
+  onActive() {
+    window.scroll(0, 0);
+  }
 }
